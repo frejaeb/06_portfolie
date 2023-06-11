5cef4b95bed6c5558b8a28129879c018d581292c
Freja <frejaeb@gmail.com>
2023-06-11 20:50:02 +0200

    com24

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// Lav variablen "btn", der henviser til ".toggle-btn"
+// variablen "btn", der henviser til ".toggle-btn"
 const btn = document.querySelector(".toggle-btn");
-// Lav variablen "menu", der henviser til ".main-menu"
+//variablen "menu", der henviser til ".main-menu"
 const menu = document.querySelector(".main-menu");
 
-// Lav funktionen "toggleMenu()" med følgende funktionalitet
+// funktionen "toggleMenu()" med følgende funktionalitet
 function toggleMenu() {
   // a) toggle klassen "shown" på menu vha. classList.toggle
   menu.classList.toggle("shown");
 
-  // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
+  // b) variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
   const menuShown = menu.classList.contains("shown");
 
   // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
